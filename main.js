9cacc6bc840f9bd32f207d6bfaea0385cc46a4ca
Txar <67731745+Txar@users.noreply.github.com>
2022-04-05 21:08:21 +0200

    fonts and content

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let material_manager = document.getElementById("material_manager")
+let materials = []
 
 class material{
     constructor(density, color, id, name){
@@ -11,8 +12,9 @@ class material{
     getMaterialDiv(){
         let id = "materialColor" + this.id
 
-        let tempMaterialString = "<div class=\"material_config\"><form><b>" + this.name + ";</b> Color: <input name=\"color\" type=\"text\" id=\""
-         + id + "\" class=\"color_input\" value=\"" + this.color + "\"></form> </div>"
+        let tempMaterialString = "<div class=\"material_config\"><form>" + this.name + "; Color: <input name=\"color\" type=\"text\" id=\""
+         + id + "\" class=\"color_input\" value=\"" + this.color + 
+         "\"> <button type=\"button\" onclick=\"removeMaterial(" + this.id + ")\">remove</button></form> </div>"
 
         return tempMaterialString
     }
@@ -20,11 +22,26 @@ class material{
 
 function load(){
     gray = new material(1.0, "FFFFFF", 0, "gray")
-    for (let i = 0; i < 20; i++){
-        material_manager.innerHTML += gray.getMaterialDiv()
+    material_manager.innerHTML += gray.getMaterialDiv()
+}
+
+function addMaterial(){
+    tempMaterial = new material(1.0, "FFFFFF", materials.length, "newMaterial")
+    materials.push(tempMaterial)
+    updateMaterials()
+}
+
+function updateMaterials(){
+    material_manager.innerHTML = ""
+    for (let i = 0; i < materials.length; i++){
+        materials[i].id = i;
+        material_manager.innerHTML += materials[i].getMaterialDiv()
     }
-    //document.getElementById("material" + gray.id).value = "FFFFFF"
-    //print(document.getElementById("material" + gray.id).value)
+}
+
+function removeMaterial(id){
+    materials.splice(id, 1)
+    updateMaterials()
 }
 
 function print(a){
